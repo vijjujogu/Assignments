bad420aad76d1cdeee6c95d3d4749eb249451006
vijayalakshmi <vijayalakshmi.jogu@atmecs.com>
2019-07-12 19:01:31 +0530

    crud operations

---
+++ SpringCRUD/src/main/java/com/example/demo/SpringCrudApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudApplication.class, args);
+	}
+
+}
+/**package org.o7planning.sbcrudrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudRestfulApplication {
+
+   public static void main(String[] args) {
+       SpringApplication.run(SpringBootCrudRestfulApplication.class, args);
+   }
+}**/
